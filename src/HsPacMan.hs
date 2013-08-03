0803eeafa55a1e168b96418d389f02865f605fb8
juffel <julian.dobmann@googlemail.com>
2013-08-03 16:45:18 +0200

    Debugggg

--- src/HsPacMan.hs
+++ src/HsPacMan.hs
@@ -2,16 +2,14 @@ module Main where
 
 import GameData
 import LevelGenerator
+import Vector2D
 
 import Graphics.Gloss hiding(display)
 import qualified Graphics.Gloss as G
 
-progName = "hsPacMan"
-
-windowTitle = progName
-windowSize = (800,600)
-windowPos = (10,10)
-
+windowTitle = "hsPacMan"
+windowPos = (100, 100) :: Size
+windowSize = (800, 600) :: Size
 
 main = play
 	display
@@ -26,21 +24,11 @@ display = InWindow windowTitle windowSize windowPos
 bgColour = black
 framerate = 40
 
-{-data DisplaySettings = DisplaySettings {
-	windowPos :: Pos,
-	windowSize :: Size
-}
-
-dispSettings = DisplaySettings {
-    windowPos = (100, 100),
-    windowSize = (400, 300)
-}--}
-
 startWorld = undefined
 
 renderWorld :: World -> Picture
-renderWorld world = case (uiState . setings world) of -- check wether the game is currently in status Playing or Menu
-    Playing ->  (uncurry Translate) (i2C (0,0)) $ -- Translation
+renderWorld world = case (uiState $ settings world) of -- check wether the game is currently in status Playing or Menu
+    Playing ->  (uncurry Translate) (i2C (fromIntegral 0, fromIntegral 0)) $ -- Translation
                 Scale 1 (-1) $ -- flip y-axis
                 renderPlaying world
 
@@ -50,11 +38,13 @@ renderPlaying :: World -> Picture
 renderPlaying = undefined
 
 renderMenu :: World -> Picture
-renderMenu = Pictures [Text "Pacman - The Menu", renderBg  ]
+renderMenu _ = Pictures [ Text "Pacman - The Menu", renderBg windowSize ]
 
-renderBg :: DisplaySettings -> Picture
-renderBg set = Color black $ Polygon path
-    where   (sizeX, sizeY) = windowSize set
+renderBg :: Size -> Picture
+renderBg (sX, sY) = Color black $ Polygon path
+    where
+            sizeX = fromIntegral sX
+            sizeY = fromIntegral sY
             path = [ (0,0), (sizeX, 0), (sizeX, sizeY), (0, sizeY), (0,0) ]
 
 handleInput = undefined
@@ -63,5 +53,5 @@ moveWorld = undefined
 -- ingame top-left-zero coordinates to gloss centered-zero coordinates
 i2C :: Vec Float -> Vec Float
 i2C (x, y) = (transfX, transfY)
-    where   transpX = x -(fst windowSize)/2 -- simple translation from [0, width] -> [-width/2, +width/2]
-            transpY = -(y -(snd windowSize)/2) -- translation plus flip from [0, height] -> [+height/2, -height/2]
+    where   transfX = x -(fromIntegral $ fst windowSize)/2 -- simple translation from [0, width] -> [-width/2, +width/2]
+            transfY = -(y -(fromIntegral $ snd windowSize)/2) -- translation plus flip from [0, height] -> [+height/2, -height/2]
